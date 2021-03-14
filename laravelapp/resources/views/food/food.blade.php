580a29048670872474aec1aadd90272fb2ab5065
Yamamoto Ryosuke <rxatv31191@yahoo.co.jp>
2021-03-14 18:17:21 +0900

    ログイン、登録ページの日本語化完了

--- laravelapp/resources/views/food/food.blade.php
+++ laravelapp/resources/views/food/food.blade.php
@@ -6,7 +6,7 @@
     <h1>食材一覧</h1>
     <p id="user-name">ユーザー名：{{$user->name . ' (' . $user->email . ')'}}</p>
     <button id="btn-add" class="btn btn-primary">
-        <a href="{{route('food_add')}}" class="text-white">食材追加</a>
+        <a href="{{route('food_add')}}" class="text-white">食材追加画面へ→</a>
     </button>
     <table align="center">
     <tr><th>食材名</th><th>数量</th><th>賞味期限</th><th>その他</th></tr>
@@ -15,8 +15,10 @@
              <td>{{$item->name}}</td>
              <td>{{$item->quantity}}{{$item->unit}}</td>
 	     <td>{{$item->limit}}</td>
-             <td><a href="{{route('food_edit')}}?id={{$item->id}}" class="btn btn-success">変更</a>
-                 <a href="{{route('food_delete')}}?id={{$item->id}}" class="btn btn-danger">削除</a></td>
+	     <td><a href="{{route('food_edit')}}?id={{$item->id}}" 
+                     class="btn btn-success">変更</a>
+		 <a href="{{route('food_delete')}}?id={{$item->id}}" 
+                     class="btn btn-danger">削除</a></td>
 	</tr>
 	@endforeach
     </table>
